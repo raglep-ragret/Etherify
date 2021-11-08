c78bca9906b38fb8b1409ed66e3503ceb07c2e67
~raglep-ragret <raglep-ragret@protonmail.com>
2021-11-08 18:05:53 -0500

    improve dark mode

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,12 +2,15 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import "tailwindcss/tailwind.css";
 import { AppProps } from "next/dist/shared/lib/router/router";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ThemeProvider attribute="class">
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
   );
 }
 
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,7 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
+import { useTheme } from "next-themes";
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
-import DarkModeToggle from "react-dark-mode-toggle";
 import AddTrackCard from "../components/AddTrackCard";
 import Button from "../components/Button";
 import Playlist from "../components/Playlist";
@@ -11,7 +12,11 @@ import {
 } from "../redux/slices/web3Slice";
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(() => false);
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  const toggleDarkMode = () =>
+    setTheme(theme === "dark" || resolvedTheme === "dark" ? "light" : "dark");
 
   const dispatch = useAppDispatch();
 
@@ -20,57 +25,75 @@ export default function Home() {
   const authorize = () => dispatch(connectWallet());
 
   useEffect(() => {
+    setMounted(true);
     authorize();
   }, []);
 
   return (
-    <div className={`${isDarkMode && "dark"}`}>
-      <div className="bg-white dark:bg-gray-900 flex flex-col items-center justify-center min-h-screen">
-        <Head>
-          <title>Etherify</title>
-          <meta
-            name="description"
-            content="A distributed Spotify playlist on the Ethereum blockchain."
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div className="bg-white dark:bg-gray-900 flex flex-col items-center justify-center min-h-screen">
+      <Head>
+        <title>Etherify</title>
+        <meta
+          name="description"
+          content="A distributed Spotify playlist on the Ethereum blockchain."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <div className="w-full flex flex-row justify-end py-2 px-2">
-          <DarkModeToggle
-            onChange={setIsDarkMode}
-            checked={isDarkMode}
-            size={80}
-          />
-        </div>
-
-        <main className="text-black dark:text-gray-50 flex flex-col items-center justify-center w-full flex-1 lg:px-20 px-8 text-center">
-          <h1 className="text-6xl font-bold">
-            Welcome to <span className="text-green-400">Etherify</span>!
-          </h1>
-
-          <p className="mt-3 text-2xl">
-            Etherify is a decentralized Spotify playlist on the Ethereun
-            blockchain, built by{" "}
-            <a
-              className="font-mono text-green-400"
-              href="https://github.com/raglep-ragret"
-              target="_blank"
+      <div className="w-full flex flex-row justify-end py-2 px-2">
+        {mounted ? (
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-8 h-8 p-1 ml-1 mr-1 rounded sm:ml-4"
+            onClick={toggleDarkMode}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="text-gray-900 dark:text-gray-100"
             >
-              ~raglep-ragret
-            </a>
-            .
-          </p>
+              {theme === "dark" || resolvedTheme === "dark" ? (
+                <SunIcon />
+              ) : (
+                <MoonIcon />
+              )}
+            </svg>
+          </button>
+        ) : (
+          <MoonIcon />
+        )}
+      </div>
 
-          {!maybeAuthorizedWallet && (
-            <Button text="Connect Wallet" onClick={authorize} />
-          )}
+      <main className="text-black dark:text-gray-50 flex flex-col items-center justify-center w-full flex-1 lg:px-20 px-8 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to <span className="text-green-400">Etherify</span>!
+        </h1>
 
-          <AddTrackCard />
+        <p className="mt-3 text-2xl">
+          Etherify is a decentralized Spotify playlist on the Ethereun
+          blockchain, built by{" "}
+          <a
+            className="font-mono text-green-400"
+            href="https://github.com/raglep-ragret"
+            target="_blank"
+          >
+            ~raglep-ragret
+          </a>
+          .
+        </p>
 
-          <Playlist />
-        </main>
+        {!maybeAuthorizedWallet && (
+          <Button text="Connect Wallet" onClick={authorize} />
+        )}
 
-        {/*
+        <AddTrackCard />
+
+        <Playlist />
+      </main>
+
+      {/*
       <footer className="text-black dark:text-gray-50 flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
@@ -83,7 +106,6 @@ export default function Home() {
         </a>
   </footer>
   */}
-      </div>
     </div>
   );
 }
